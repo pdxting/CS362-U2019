34791266342821ad3d3cdb21ee6976840a14fcba
Ting Sheppy <chit@oregonstate.edu>
2019-07-07 23:05:40 -0700

    Assignment 2 complete

--- chit/dominion/dominion_helpers.h
+++ chit/dominion/dominion_helpers.h
@@ -12,4 +12,16 @@ int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
 
+int baronAction(struct gameState * state, int currentPlayer, int choice1);
+
+int minionAction(struct gameState * state, int currentPlayer, int choice1, int choice2, int handPos);
+
+int ambassadorAction(struct gameState * state, int currentPlayer, int choice1, int choice2, int handPos);
+
+int tributeAction(struct gameState * state, int currentPlayer, int nextPlayer);
+int setCard(struct gameState * state, int nextPlayer, int card);
+void giveCardToPlayer(struct gameState * state, int currentPlayer, int card);
+
+int mineAction(struct gameState * state, int currentPlayer, int choice1, int choice2, int handPos);
+
 #endif
