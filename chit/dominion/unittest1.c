ed9ade67016c6514e4df83830c16640e26f7fa6b
Ting Sheppy <chit@oregonstate.edu>
2019-07-13 18:24:05 -0700

    Working on coverage

---
+++ chit/dominion/unittest1.c
@@ -0,0 +1,82 @@
+/*
+ * unittest1.c
+ *
+ */
+
+/*
+ * Include the following lines in your makefile:
+ *
+ * unittest1: unittest1.c dominion.o rngs.o
+ *      gcc -o unittest1 -g  unittest1.c dominion.o rngs.o $(CFLAGS)
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+#define TESTCARD "Baron"
+
+void assertCheck(int A, int B, int testNum);
+
+int main() {
+
+    int choice1 = 0;
+    int seed = 1000;
+    int numPlayers = 2;
+    int thisPlayer = 0;
+	struct gameState beforeState, afterState;
+	int k[10] = {baron, minion, ambassador, embargo, tribute, mine, cutpurse, remodel, smithy, village };
+
+	initializeGame(numPlayers, k, seed, &beforeState);
+
+	printf("\n----------------- Testing Card: %s ----------------\n", TESTCARD);
+
+	// ----- Test 1 -----
+	printf("TEST 1: No estate found, check if player gets an estate card and supply count is correct\n");
+
+	memcpy(&afterState, &beforeState, sizeof(struct gameState));
+	choice1 = 0;
+	baronAction(&afterState, thisPlayer, choice1);
+
+	printf("Estate card supply before count: ");
+	printf("%d\n", beforeState.supplyCount[estate]);
+	printf("Estate card supply after count: ");
+	printf("%d\n", afterState.supplyCount[estate]); 
+	printf("Expecting one less card after estate card passed out to player\n");
+
+	assertCheck(afterState.supplyCount[estate], beforeState.supplyCount[estate] - 1, 1);
+
+	// ----- Test 2 -----
+	printf("TEST 2: There's an estate card, check to see if player gets coins\n");
+
+	beforeState.hand[thisPlayer][0] = baron;
+	beforeState.hand[thisPlayer][1] = copper;
+	beforeState.hand[thisPlayer][2] = remodel;
+	beforeState.hand[thisPlayer][3] = estate;
+	beforeState.hand[thisPlayer][4] = copper;
+
+	memcpy(&afterState, &beforeState, sizeof(struct gameState));
+	choice1 = 1;
+
+	baronAction(&afterState, thisPlayer, choice1);
+
+	printf("Coin count = %d, Expected = %d\n", afterState.coins, beforeState.coins + 4);
+	assertCheck(afterState.coins, beforeState.coins + 4, 2);
+
+
+	return 0;
+}
+
+void assertCheck(int A, int B, int testNum)
+{
+	if (A == B)
+		printf("Test %d passed\n\n", testNum);
+	else
+		printf("Test %d failed\n\n", testNum);
+}
+
+
--- dominion/dominion.h
+++ dominion/dominion.h
@@ -25,29 +25,27 @@ enum CARD
    silver,
    gold,
 
-   adventurer,
-   /* If no/only 1 treasure found, stop when full deck seen */
+   adventurer,	/* If no/only 1 treasure found, stop when full deck seen */
    council_room,
-   feast, /* choice1 is supply # of card gained) */
+   feast,		/* choice1 is supply # of card gained) */
    gardens,
-   mine, /* choice1 is hand# of money to trash, choice2 is supply# of
-	    money to put in hand */
-   remodel, /* choice1 is hand# of card to remodel, choice2 is supply# */
+   mine,		/* choice1 is hand# of money to trash, choice2 is supply# of money to put in hand */
+   remodel,		/* choice1 is hand# of card to remodel, choice2 is supply# */
    smithy,
    village,
 
-   baron, /* choice1: boolean for discard of estate */
-   /* Discard is always of first (lowest index) estate */
+   baron,		/* choice1: boolean for discard of estate */
+				/* Discard is always of first (lowest index) estate */
    great_hall,
-   minion, /* choice1:  1 = +2 coin, 2 = redraw */
-   steward, /* choice1: 1 = +2 card, 2 = +2 coin, 3 = trash 2 (choice2,3) */
+   minion,		/* choice1: 1 = +2 coin, 2 = redraw */
+   steward,		/* choice1: 1 = +2 card, 2 = +2 coin, 3 = trash 2 (choice2,3) */
    tribute,
 
-   ambassador, /* choice1 = hand#, choice2 = number to return to supply */
+   ambassador,	/* choice1 = hand#, choice2 = number to return to supply */
    cutpurse,
-   embargo, /* choice1 = supply# */
+   embargo,		/* choice1 = supply# */
    outpost,
-   salvager, /* choice1 = hand# to trash */
+   salvager,	/* choice1 = hand# to trash */
    sea_hag,
    treasure_map
   };
